d0c5dd39cb5293f5fa5977fff1f052ae50da37a2
alfardan59 <alfardanarfin500@gmail.com>
2025-01-08 23:13:30 +0530

    final commit --all changes saved

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 60,
+        }}
+      >
+        <p style={{ fontSize: "30px", textAlign: "center", padding: "20px" }}>
+          Built With love by
+          <span>
+              Alfardan Arfin
+          </span>
+          💘
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
